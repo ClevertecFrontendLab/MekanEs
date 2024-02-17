fba133734371420ceef3e910a0145276db0c36e5
MekanEs <Mekan.es1997@gmail.com>
2024-02-17 23:24:48 +0300

    feat: add Paths enum

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -2,19 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import { Auth, MainPage } from '@pages/index';
 import { AuthProvider } from '@modules/auth';
 import { AppLayout } from '@shared/components';
+import { Paths } from '@shared/types/common';
 
 export const routes = (
     <Routes>
-        <Route path='/' element={<AppLayout />}>
-            <Route element={<AuthProvider passIf={false} redirect='/auth' />}>
-                <Route path='/main' element={<MainPage />} />
+        <Route path={Paths.BASE} element={<AppLayout />}>
+            <Route element={<AuthProvider passIf={false} redirect={Paths.AUTH} />}>
+                <Route path={Paths.MAIN} element={<MainPage />} />
             </Route>
-
-            <Route path='/second' element={<>second</>} />
         </Route>
-        <Route element={<AuthProvider withLayout passIf={true} redirect='/main' />}>
-            <Route path='/auth/registration' element={<Auth />} />
-            <Route path='/auth' element={<Auth />} />
+        <Route element={<AuthProvider withLayout passIf={true} redirect={Paths.MAIN} />}>
+            <Route path={Paths.REGISTRATION} element={<Auth />} />
+            <Route path={Paths.AUTH} element={<Auth />} />
         </Route>
     </Routes>
 );
